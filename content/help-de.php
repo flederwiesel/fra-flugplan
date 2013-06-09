694b54dc02e5107291f8be2fbce3106556ba562b
flederwiesel <flederwiesel@fra-flugplan.de>
2013-06-09 19:44:27 +0000

    Added help.

---
+++ content/help-de.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang_help = array(
+	0 => 'Nach oben',
+	1 => 'Hilfe - Inhalt',
+	2 => 'Grundlegende Bedienung',
+	3 => 'Beobachtungsliste',
+	4 => 'Beobachtungsliste definieren',
+	5 => 'Sortieren',
+	6 => 'Nach Kennungen sortieren',
+	7 => 'Mobile Geräte',
+	8 => 'Probleme?',
+	9 => '(1) Ankunft oder Abflug zur Ansicht auswählen',
+	10 => '(2) Auswahl wird in der Überschrift angezeigt',
+	11 => '(3) Die <a href="http://de.wikipedia.org/wiki/Start-_und_Landebahn#Landebahnkennung">Betriebsichtung</a> wird im roten Kasten angezeigt',
+	12 => '(4) Flugzeiten werden im Format <span class="b">"+/-Tag Stunde:Minuten"</span> angezeigt, dabei bedeutet +0 heute, +1 bedeutet morgen, usw.&#20;',
+	13 => '(5) Zeiten für verfrühte Flüge werden in rot angezeigt',
+	14 => '(6) Flüge in der Vergangenheit werden ausgegraut angezeigt',
+	15 => '(7) Flugzeugkennungen sind mit <a href="http://www.airliners.net">www.airliners.net</a> verlinkt (auf Symbol links davon klicken)',
+	16 => '(8) Registrieren/Anmelden, um die <span class="bi">Beobachtungsliste</span> nutzen zu können',
+	17 => '(1) Besondere Flugzeuge können in grün hervorgehoben werden. Eine Liste davon  - inklusive einem Kommentar je Kennung - kann im Benutzerprofil gespeichert werden',
+	18 => '(2) Auf den Reiter oder den Link in der Navigationsleiste klicken, damit die Beobachtungsliste geöffnet wird. Diese Liste kann nach Belieben bearbeitet werden',
+	19 => '<span class="b">Um die Beobachtungsliste benutzen zu können, muß <span class="i"><a href="http://www.enable-javascript.com/de/">JavaScript aktiviert</a></span> sein.</span>',
+	20 => 'Kennung und Kommentar eintragen,',
+	21 => '(1) Hinzufügen-Schaltfläche benutzen, um neue Zeilen einzufügen',
+	22 => '(2) Einträge, die nicht länger benötigt werden, können gelöscht werden',
+	23 => '(3) Aktualisieren speichert aktuelle Listeneinträge',
+	24 => 'Im Tabellenkopf auf eine Spalte klicken, um nach dieser zu sortieren',
+	25 => 'Bei aktiver Beobachtungsliste (Anmelden erforderlich) werden die markierten Flugzeuge zuoberst sortiert',
+	26 => 'Für die ordentliche Unterstützung mobiler Endgeräte gibt es noch einiges zu tun. Sorry. Unterstützung für spezielle Geräte/Browser kann jederzeit <a href="mailto:hausmeister(&auml;t)flederwiesel.com&subject=fra-schedule inquiry">angefragt</a><a href="#at"><sup style="font-style: italic;">[1]</sup></a> werden',
+	27 => 'Keine Software ist fehlerfrei. Diese Seite wurde getestet, so gut es innerhalb der Grenzen meiner Freizeit möglich war. Sollten trotzdem Probleme oder Fragen auftauchen, die über diese Hilfeseite hinausgehen, bitte <a href="mailto:hausmeister(&auml;t)flederwiesel.com&subject=fra-schedule trouble">Kontakt</a><a href="#at"><sup style="font-style: italic;">[1]</sup></a> mit mir aufnehmen',
+	28 => '(&auml;t) in der E-Mailadresse durch @ ersetzen',
+);
+
+?>
---
+++ content/help-en.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang_help = array(
+	0 => '[Back to top]',
+	1 => 'Help - Contents',
+	2 => 'Basic usage',
+	3 => 'Watchlist',
+	4 => 'Defining a watchlist',
+	5 => 'Sorting',
+	6 => 'Sorting registrations',
+	7 => 'Mobile devices',
+	8 => 'Got trouble?',
+	9 => '(1) Select whether you want to view arriving or departing flights',
+	10 => '(2) In the headline this selection will be shown',
+	11 => '(3) The <a href="http://en.wikipedia.org/wiki/Runways#Orientation_and_dimensions">runway(s) in operation</a> will be shown in the red box',
+	12 => '(4) Flight times will be shown in format <span class="b">"+/-day hour:minutes"</span>, where +0 means today, +1 is tomorrow, and so on',
+	13 => '(5) Premature flights are indicated by times in red',
+	14 => '(6) Flights in the past will be grayed out',
+	15 => '(7) Registrations are linked to <a href="http://www.airliners.net">www.airliners.net</a> (click on the icon to the left)',
+	16 => '(8) Register/Login to benefit from the <span class="bi">watchlist</span> feature',
+	17 => '(1) Let your favourite aircrafts be hilighted in green. A list thereof - including a comment per aircraft - will be stored within your user profile',
+	18 => '(2) Click onto the watchlist tab or the link in the navigation area to let it slide into view. This list can be edited as you wish',
+	19 => '<span class="b">The watchlist feature requires <span class="i"><a href="http://www.enable-javascript.com/">JavaScript to be enabled</a></span>.</span>',
+	20 => 'Enter registration/comment pairs,',
+	21 => '(1) use the Add button to add lines to the table',
+	22 => '(2) You can also remove registrations if you do not want them ony your watchlist any longer',
+	23 => '(3) Press Refresh to commit the changes',
+	24 => 'Click one of the column headers to sort by a certain criteria',
+	25 => 'If you are logged in and have defined a watchlist, the watched aircrafts will be sorted first',
+	26 => 'For decent support of mobile devices there is still some work to do. Sorry. If you want support for a specific device/browser, do not hestitate to <a href="mailto:hausmeister(&auml;t)flederwiesel.com&subject=fra-schedule inquiry">send an inquiry</a><a href="#at"><sup style="font-style: italic;">[1]</sup></a>',
+	27 => 'Software is never error-free. I tested this site as much as possible within the limits of my spare time. If you however, have trouble using the site or need further help than is provided here, feel free to <a href="mailto:hausmeister(&auml;t)flederwiesel.com&subject=fra-schedule trouble">Contact me</a><a href="#at"><sup style="font-style: italic;">[1]</sup></a>',
+	28 => 'Contact: hausmeister(&auml;t)flederwiesel.com. Replace the (&auml;t) in the e-mail address with @',
+);
+
+?>
