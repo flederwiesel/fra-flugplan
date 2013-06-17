caf408ebe2b68c678a89f3347c2394ed38badd7e
flederwiesel <flederwiesel@fra-flugplan.de>
2013-06-17 19:22:42 +0000

    Added variables for root and target. target revision  file will now be generated. Fixed --exclude

--- ftp-flederwiesel.sh
+++ ftp-flederwiesel.sh
@@ -1,36 +1,40 @@
 #!/bin/sh
 
-rev=$(svn info . | awk '/^Revision:/ { print $2; }')
+root=/html
+target=fra-schedule
 
 [ -e .config ] || cp -f .config.flederwiesel .config
+
+rev=$(svn info . | awk '/^Revision:/ { print $2; }')
 
 lftp <<EOF
 open web416:L2ppkt1fl2@www.flederwiesel.com
 
-!svn info . | awk '/^(URL|Revision):/ { print $2; }' > revision
+!svn info . | awk '/^URL:/ { print $2; }' > revision
 
-rm -rf /html/vault/fra-schedule/$rev
-mkdir -p /html/vault/fra-schedule/$rev
-cd /html/vault/fra-schedule/$rev
-
-lpwd
+rm -rf ${root}/vault/${target}/$rev
+mkdir -p ${root}/vault/${target}/$rev
+cd ${root}/vault/${target}/$rev
 
 mirror --reverse \
 	--verbose \
 	--no-perms \
-	--exclude .config.*/ \
-	--exclude .svn/ \
-	--exclude check/ \
-	--exclude data/betriebsrichtung.* \
-	--exclude etc/ \
-	--exclude ftp-flederwiesel.lftp \
-	--exclude ftp-lorem-ipsum.lftp \
-	--exclude sql/ \
-	--exclude METAR
+	--exclude-glob *.sql.gz \
+	--exclude-glob .config.* \
+	--exclude-glob data/*.* \
+	--exclude-glob ftp-*.sh \
+	--exclude .svn \
+	--exclude check \
+	--exclude etc \
+	--exclude sql \
+	--exclude METAR \
+	--exclude ToDo.txt
 #	--dry-run \
 
-#cd /html/vault/fra-schedule
-#!echo $rev > recent
+!echo $rev > target
+cd ${root}/vault/${target}
+put target
+!rm target
 
 bye
 EOF
--- ftp-lorem-ipsum.sh
+++ ftp-lorem-ipsum.sh
@@ -1,36 +1,40 @@
 #!/bin/sh
 
-rev=$(svn info . | awk '/^Revision:/ { print $2; }')
+root=/html
+target=lorem-ipsum
 
 [ -e .config ] || cp -f .config.lorem-ipsum .config
+
+rev=$(svn info . | awk '/^Revision:/ { print $2; }')
 
 lftp <<EOF
 open web416:L2ppkt1fl2@www.flederwiesel.com
 
-!svn info . | awk '/^(URL|Revision):/ { print $2; }' > revision
+!svn info . | awk '/^URL:/ { print $2; }' > revision
 
-rm -rf /html/vault/lorem-ipsum/$rev
-mkdir -p /html/vault/lorem-ipsum/$rev
-cd /html/vault/lorem-ipsum/$rev
-
-lpwd
+rm -rf ${root}/vault/${target}/$rev
+mkdir -p ${root}/vault/${target}/$rev
+cd ${root}/vault/${target}/$rev
 
 mirror --reverse \
 	--verbose \
 	--no-perms \
-	--exclude .config.*/ \
-	--exclude .svn/ \
-	--exclude check/ \
-	--exclude data/betriebsrichtung.* \
-	--exclude etc/ \
-	--exclude ftp-flederwiesel.lftp \
-	--exclude ftp-lorem-ipsum.lftp \
-	--exclude sql/ \
-	--exclude METAR
+	--exclude-glob *.sql.gz \
+	--exclude-glob .config.* \
+	--exclude-glob data/*.* \
+	--exclude-glob ftp-*.sh \
+	--exclude .svn \
+	--exclude check \
+	--exclude etc \
+	--exclude sql \
+	--exclude METAR \
+	--exclude ToDo.txt
 #	--dry-run \
 
-#cd /html/vault/lorem-ipsum
-#!echo $rev > recent
+!echo $rev > target
+cd ${root}/vault/${target}
+put target
+!rm target
 
 bye
 EOF
