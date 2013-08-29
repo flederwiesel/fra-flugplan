61556053f8fcd3675d4a655600e2fd363e9516e3
flederwiesel <flederwiesel@fra-flugplan.de>
2013-08-29 19:01:47 +0000

    Added header comment, fixed encoding.

--- content/help-de.php
+++ content/help-de.php
@@ -1,5 +1,27 @@
 <?php
 
+/******************************************************************************
+ *
+ *       project: FRA-flights Live Schedule
+ *
+ *       $Author: flederwiesel $
+ *         $Date: 2013-03-25 20:06:24 +0100 (Mo, 25 Mrz 2013) $
+ *          $Rev: 208 $
+ *
+ ******************************************************************************
+ *
+ * Copyright © Tobias Kühne
+ *
+ * You may use and distribute this software free of charge for non-commercial
+ * purposes. The software must be distributed in its entirety, i.e. containing
+ * ALL binary and source files without modification.
+ * Publication of modified versions of the source code provided herein,
+ * is permitted only with the author's written consent. In this case the
+ * copyright notice must not be removed or altered, all modifications to the
+ * source code must be clearly marked as such.
+ *
+ ******************************************************************************/
+
 $lang_help = array(
 	0 => 'Nach oben',
 	1 => 'Hilfe - Inhalt',
@@ -8,27 +30,27 @@ $lang_help = array(
 	4 => 'Beobachtungsliste definieren',
 	5 => 'Sortieren',
 	6 => 'Nach Kennungen sortieren',
-	7 => 'Mobile Geräte',
+	7 => 'Mobile GerÃ¤te',
 	8 => 'Probleme?',
-	9 => '(1) Ankunft oder Abflug zur Ansicht auswählen',
-	10 => '(2) Auswahl wird in der Überschrift angezeigt',
+	9 => '(1) Ankunft oder Abflug zur Ansicht auswÃ¤hlen',
+	10 => '(2) Auswahl wird in der Ãœberschrift angezeigt',
 	11 => '(3) Die <a href="http://de.wikipedia.org/wiki/Start-_und_Landebahn#Landebahnkennung">Betriebsrichtung</a> wird im roten Kasten angezeigt',
 	12 => '(4) Flugzeiten werden im Format <span class="b">"+/-Tag Stunde:Minuten"</span> angezeigt, dabei bedeutet +0 heute, +1 bedeutet morgen, usw.&#20;',
-	13 => '(5) Zeiten für verfrühte Flüge werden in rot angezeigt',
-	14 => '(6) Flüge in der Vergangenheit werden ausgegraut angezeigt',
+	13 => '(5) Zeiten fÃ¼r verfrÃ¼hte FlÃ¼ge werden in rot angezeigt',
+	14 => '(6) FlÃ¼ge in der Vergangenheit werden ausgegraut angezeigt',
 	15 => '(7) Flugzeugkennungen sind mit <a href="http://www.airliners.net">www.airliners.net</a> verlinkt (auf Symbol links davon klicken)',
-	16 => '(8) Registrieren/Anmelden, um die <span class="bi">Beobachtungsliste</span> nutzen zu können',
-	17 => '(1) Besondere Flugzeuge können in grün hervorgehoben werden. Eine Liste davon  - inklusive einem Kommentar je Kennung - kann im Benutzerprofil gespeichert werden',
-	18 => '(2) Auf den Reiter oder den Link in der Navigationsleiste klicken, damit die Beobachtungsliste geöffnet wird. Diese Liste kann nach Belieben bearbeitet werden',
-	19 => '<span class="b">Um die Beobachtungsliste benutzen zu können, muß <span class="i"><a href="http://www.enable-javascript.com/de/">JavaScript aktiviert</a></span> sein.</span>',
+	16 => '(8) Registrieren/Anmelden, um die <span class="bi">Beobachtungsliste</span> nutzen zu kÃ¶nnen',
+	17 => '(1) Besondere Flugzeuge kÃ¶nnen in grÃ¼n hervorgehoben werden. Eine Liste davon  - inklusive einem Kommentar je Kennung - kann im Benutzerprofil gespeichert werden',
+	18 => '(2) Auf den Reiter oder den Link in der Navigationsleiste klicken, damit die Beobachtungsliste geÃ¶ffnet wird. Diese Liste kann nach Belieben bearbeitet werden',
+	19 => '<span class="b">Um die Beobachtungsliste benutzen zu kÃ¶nnen, muÃŸ <span class="i"><a href="http://www.enable-javascript.com/de/">JavaScript aktiviert</a></span> sein.</span>',
 	20 => 'Kennung und Kommentar eintragen,',
-	21 => '(1) Hinzufügen-Schaltfläche benutzen, um neue Zeilen einzufügen',
-	22 => '(2) Einträge, die nicht länger benötigt werden, können gelöscht werden',
-	23 => '(3) Aktualisieren speichert aktuelle Listeneinträge',
+	21 => '(1) HinzufÃ¼gen-SchaltflÃ¤che benutzen, um neue Zeilen einzufÃ¼gen',
+	22 => '(2) EintrÃ¤ge, die nicht lÃ¤nger benÃ¶tigt werden, kÃ¶nnen gelÃ¶scht werden',
+	23 => '(3) Aktualisieren speichert aktuelle ListeneintrÃ¤ge',
 	24 => 'Im Tabellenkopf auf eine Spalte klicken, um nach dieser zu sortieren',
 	25 => 'Bei aktiver Beobachtungsliste (Anmelden erforderlich) werden die markierten Flugzeuge zuoberst sortiert',
-	26 => 'Für die ordentliche Unterstützung mobiler Endgeräte gibt es noch einiges zu tun. Sorry. Unterstützung für spezielle Geräte/Browser kann jederzeit unter <a href="content/emil.php?subject=FRA-schedule%20Anfrage"><img alt="email" src="content/emil-img.php" style="vertical-align: bottom;"></a> angefragt</a> werden',
-	27 => 'Keine Software ist fehlerfrei. Diese Seite wurde getestet, so gut es innerhalb der Grenzen meiner Freizeit möglich war. Sollten trotzdem Probleme oder Fragen auftauchen, die über diese Hilfeseite hinausgehen, bitte Kontakt mit mir unter <a href="content/emil.php?subject=FRA-schedule%20Problem"><img alt="email" src="content/emil-img.php" style="vertical-align: bottom;"></a> aufnehmen',
+	26 => 'FÃ¼r die ordentliche UnterstÃ¼tzung mobiler EndgerÃ¤te gibt es noch einiges zu tun. Sorry. UnterstÃ¼tzung fÃ¼r spezielle GerÃ¤te/Browser kann jederzeit unter <a href="content/emil.php?subject=FRA-schedule%20Anfrage"><img alt="email" src="content/emil-img.php" style="vertical-align: bottom;"></a> angefragt</a> werden',
+	27 => 'Keine Software ist fehlerfrei. Diese Seite wurde getestet, so gut es innerhalb der Grenzen meiner Freizeit mÃ¶glich war. Sollten trotzdem Probleme oder Fragen auftauchen, die Ã¼ber diese Hilfeseite hinausgehen, bitte Kontakt mit mir unter <a href="content/emil.php?subject=FRA-schedule%20Problem"><img alt="email" src="content/emil-img.php" style="vertical-align: bottom;"></a> aufnehmen',
 );
 
 ?>
--- content/help-en.php
+++ content/help-en.php
@@ -1,5 +1,27 @@
 <?php
 
+/******************************************************************************
+ *
+ *       project: FRA-flights Live Schedule
+ *
+ *       $Author: flederwiesel $
+ *         $Date: 2013-03-25 20:06:24 +0100 (Mo, 25 Mrz 2013) $
+ *          $Rev: 208 $
+ *
+ ******************************************************************************
+ *
+ * Copyright © Tobias Kühne
+ *
+ * You may use and distribute this software free of charge for non-commercial
+ * purposes. The software must be distributed in its entirety, i.e. containing
+ * ALL binary and source files without modification.
+ * Publication of modified versions of the source code provided herein,
+ * is permitted only with the author's written consent. In this case the
+ * copyright notice must not be removed or altered, all modifications to the
+ * source code must be clearly marked as such.
+ *
+ ******************************************************************************/
+
 $lang_help = array(
 	0 => '[Back to top]',
 	1 => 'Help - Contents',
