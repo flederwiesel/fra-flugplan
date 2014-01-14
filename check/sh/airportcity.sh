3bf7c806c2555bfa19a2cf11672d8f511ae6fcb4
flederwiesel <flederwiesel@fra-flugplan.de>
2014-01-14 02:05:14 +0000

    Dates will now be substituted with offset.

--- check/sh/airportcity.sh
+++ check/sh/airportcity.sh
@@ -21,7 +21,19 @@ do
 
 	for time in {5..23}
 	do
+
 		echo $(printf '%u %02u:00' $day $time) > flugplan/airportcity/querytime
+
+		ddmmYYYY_0=$(date +'%d\.%m\.%Y')
+		ddmmYYYY_1=$(date +'%d\.%m\.%Y' --date="+1 day")
+		ddmmYYYY_2=$(date +'%d\.%m\.%Y' --date="+2 days")
+		dd_mm_YYYY_0=$(date +%d-%m-%Y)
+		dd_mm_YYYY_1=$(date +%d-%m-%Y --date="+1 day")
+		dd_mm_YYYY_2=$(date +%d-%m-%Y --date="+2 days")
+		YYYYmmdd_0=$(date +%Y%m%d)
+		YYYYmmdd_1=$(date +%Y%m%d --date="+1 day")
+		YYYYmmdd_2=$(date +%Y%m%d --date="+2 days")
+
 		page=1
 
 		while [ $page -gt 0 ]
@@ -38,13 +50,49 @@ do
 				}
 				' <<<"$htm")
 
-			check $(printf '%u-%02u00-arrival-%u' $day $time $page) "echo '$htm'"
+			check $(printf '%u-%02u00-arrival-%u' $day $time $page) "echo '$htm'" \
+				"| sed -r '
+					s#/dd_mm_YYYY_0/#/00-00-0000/#g
+					s#/dd_mm_YYYY_1/#/01-00-0000/#g
+					s#/dd_mm_YYYY_2/#/02-00-0000/#g
+					s/$ddmmYYYY_0/00.00.0000/g
+					s/$ddmmYYYY_1/01.00.0000/g
+					s/$ddmmYYYY_2/02.00.0000/g
+					s/$dd_mm_YYYY_0/00-00-0000/g
+					s/$dd_mm_YYYY_1/01-00-0000/g
+					s/$dd_mm_YYYY_2/02-00-0000/g
+					s/(fi[ad]=[A-Z0-9]+)$YYYYmmdd_0\&/\100000000\&/g
+					s/(fi[ad]=[A-Z0-9]+)$YYYYmmdd_1\&/\100000001\&/g
+					s/(fi[ad]=[A-Z0-9]+)$YYYYmmdd_2\&/\100000002\&/g
+				'"
+
 			page=$next
+
 		done
-
-		YYYYmmdd=$(date +%Y%m%d --date="+$day days")
-		check $(printf '%u-%02u00-fia' $day $time) curl "$url/check/flugplan/airportcity/?fia=SA260$YYYYmmdd"
-		check $(printf '%u-%02u00-fid' $day $time) curl "$url/check/flugplan/airportcity/?fid=SA261$YYYYmmdd"
-
 	done
 done
+
+for day in {0..1}
+do
+
+	echo "$day 06:00" > flugplan/airportcity/querytime
+
+	YYYYmmdd=$(date +%Y%m%d --date="+$day days")
+
+	check "$day-fia" curl "$url/check/flugplan/airportcity/?fia=SA260$YYYYmmdd" \
+		"| sed -r 's/[0-9]{2}\\.[0-9]{2}\\.[0-9]{4}/00.00.0000/g'"
+
+	check "$day-fid" curl "$url/check/flugplan/airportcity/?fid=SA261$YYYYmmdd" \
+		"| sed -r 's/[0-9]{2}\\.[0-9]{2}\\.[0-9]{4}/00.00.0000/g'"
+
+done
+
+echo "1 23:00" > flugplan/airportcity/querytime
+
+YYYYmmdd=$(date +%Y%m%d --date="+$day days")
+
+check "$day-fia" curl "$url/check/flugplan/airportcity/?fia=SA260$YYYYmmdd" \
+	"| sed -r 's/[0-9]{2}\\.[0-9]{2}\\.[0-9]{4}/00.00.0000/g'"
+
+check "$day-fid" curl "$url/check/flugplan/airportcity/?fid=SA261$YYYYmmdd" \
+	"| sed -r 's/[0-9]{2}\\.[0-9]{2}\\.[0-9]{4}/00.00.0000/g'"
--- check/sh/getflights.sh
+++ check/sh/getflights.sh
@@ -26,9 +26,14 @@ do
 
 		time=$(printf '%02u:00' $t)
 
-		YYYYmmdd_0=$(date +'%Y%m%d' --date="$time")
-		YYYYmmdd_1=$(date +'%Y%m%d' --date="$time +1 day")
-		dHHMM=$(printf   '%02u' $day)-$(date +'%H%M' --date="$time")
+		YYYYmmdd_0=$(date +'%Y%m%d')
+		YYYYmmdd_1=$(date +'%Y%m%d' --date="+1 days")
+		YYYYmmdd_2=$(date +'%Y%m%d' --date="+2 days")
+		YYYY_mm_dd_0=$(date +'%Y-%m-%d')
+		YYYY_mm_dd_1=$(date +'%Y-%m-%d' --date="+1 days")
+		YYYY_mm_dd_2=$(date +'%Y-%m-%d' --date="+2 days")
+
+		dHHMM=$(printf '%02u' $day)-$(date +'%H%M' --date="$time")
 
 		now=$(date +'%Y-%m-%d %H:%M:%S' --date="$time")
 		now=$(rawurlencode $now)
@@ -36,18 +41,23 @@ do
 		echo "$day $time" > flugplan/airportcity/querytime
 
 		check "$dHHMM-getflights" curl "$url/getflights.php?baseurl=$baseurl\&now=$now\&debug=url,query\&fmt=html"\
-			"| sed -r '"\
-			"s/Dauer: [0-9]+.[0-9]+s/Dauer: 0.000s/g;"\
-			"s/(fi[ad]=[A-Z0-9]+)$YYYYmmdd_0$/\100000000/g;"\
-			"s/(fi[ad]=[A-Z0-9]+)$YYYYmmdd_1$/\100000000/g;"\
-			"s/[0-9]{4}-[0-9]{2}-[0-9]{2} ([0-9]{2}:[0-9]{2}(:[0-9]{2})?)/0000-00-00 \1/g"\
-			"'"
+			"| sed -r '
+			s/Dauer: [0-9]+.[0-9]+s/Dauer: 0.000s/g
+			s/(fi[ad]=[A-Z0-9]+)$YYYYmmdd_0$/\100000000/g
+			s/(fi[ad]=[A-Z0-9]+)$YYYYmmdd_1$/\100000001/g
+			s/(fi[ad]=[A-Z0-9]+)$YYYYmmdd_2$/\100000002/g
+			s/$YYYY_mm_dd_0 ([0-9]{2}:[0-9]{2}(:[0-9]{2})?)/0000-00-00 \1/g
+			s/$YYYY_mm_dd_1 ([0-9]{2}:[0-9]{2}(:[0-9]{2})?)/0000-00-01 \1/g
+			s/$YYYY_mm_dd_2 ([0-9]{2}:[0-9]{2}(:[0-9]{2})?)/0000-00-02 \1/g
+			'"
 
 		flights=$(query 'USE `fra-schedule`;
 			SELECT
 			 `flights`.`direction`,
 			 `flights`.`scheduled`,
 			 `flights`.`expected`,
+			 `airlines`.`code`,
+			 `flights`.`code`,
 			 `airports`.`iata` AS `airport:iata`,
 			 `airports`.`icao` AS `airport:icao`,
 			 `models`.`icao` AS `model`,
@@ -69,20 +79,28 @@ do
 				1 i <pre>
 				s/arrival/A/g
 				s/departure/D/g
-				s/[0-9]{4}-[0-9]{2}-[0-9]{2}/0000-00-00/g
+				s/$YYYY_mm_dd_0/0000-00-00/g
+				s/$YYYY_mm_dd_1/0000-00-01/g
+				s/$YYYY_mm_dd_2/0000-00-02/g
 				$ a </pre>
 				$ a </body>
 				$ a </html>
 			'"
 
 		check "$dHHMM-arrival" curl "$url/?arrival\&now=$now"\
-			"| sed -r 's/now=[0-9]{4}-[0-9]{2}-[0-9]{2}/now=0000-00-00/g'"
+			"| sed -r '
+				s/now=$YYYY_mm_dd_0/now=0000-00-00/g
+				s/now=$YYYY_mm_dd_1/now=0000-00-01/g
+			'"
 
 		# Note, that RARE A/C will not be marked as such,
 		# since we use different a/c in arrival/departure.csv,
 		# and only arrivals will evaluate visits
 		check "$dHHMM-departure" curl "$url/?departure\&now=$now"\
-			"| sed -r 's/now=[0-9]{4}-[0-9]{2}-[0-9]{2}/now=0000-00-00/g'"
+			"| sed -r '
+				s/now=$YYYY_mm_dd_0/now=0000-00-00/g
+				s/now=$YYYY_mm_dd_1/now=0000-00-01/g
+			'"
 
 	done
 
