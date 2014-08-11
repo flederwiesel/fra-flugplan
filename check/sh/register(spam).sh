04c8e8e983c7453a89ed200c31be8994c49af838
flederwiesel <flederwiesel@fra-flugplan.de>
2014-08-11 01:55:37 +0000

    Added test case "register(spam)".

---
+++ check/sh/register(spam).sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+###############################################################################
+#
+#  Copyright © Tobias Kühne
+#
+#  You may use and distribute this software free of charge for non-commercial
+#  purposes. The software must be distributed in its entirety, i.e. containing
+#  ALL binary and source files without modification.
+#  Publication of modified versions of the source code provided herein,
+#  is permitted only with the author's written consent. In this case the
+#  copyright notice must not be removed or altered, all modifications to the
+#  source code must be clearly marked as such.
+#
+###############################################################################
+
+# drop/re-create database
+initdb && rm -f .COOKIES
+chkmail=1
+
+###############################################################################
+
+check "1" curl "$url/?req=register" \
+		--data-urlencode "email=compactzmtn@gmail.com" \
+		--data-urlencode "user=ljdwefdsyrovtq" \
+		--data-urlencode "passwd=elvizzz" \
+		--data-urlencode "passwd-confirm=elvizzz" \
+		--data-urlencode "timezone=UTC+1" \
+		--data-urlencode "lang=en"
