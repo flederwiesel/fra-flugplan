7037ea4d46d951482df264c079ba0e0825d76005
flederwiesel <flederwiesel@fra-flugplan.de>
2014-08-20 09:44:27 +0000

    "admin.php" was missing :(

---
+++ content/admin.php
@@ -0,0 +1,62 @@
+<?php
+
+/******************************************************************************
+ *
+ * Copyright © Tobias Kühne
+ *
+ * You may use and distribute this software free of charge for non-commercial
+ * purposes. The software must be distributed in its entirety, i.e. containing
+ * ALL binary and source files without modification.
+ * Publication of modified versions of the source code provided herein,
+ * is permitted only with the author's written consent. In this case the
+ * copyright notice must not be removed or altered, all modifications to the
+ * source code must be clearly marked as such.
+ *
+ ******************************************************************************/
+
+if ('userdel' == $_GET['admin'])
+{
+	if (!$user)
+	{
+		$error = $lang['notloggedin'];//.': '.htmlget();
+	}
+	else
+	{
+		$perms = $user->permissions();
+
+		if (0 == $perms[0])
+		{
+			$error = $lang['nopermission'];
+		}
+		else
+		{
+			$result = mysql_query("DELETE FROM `users` WHERE `id`=$_GET[uid]");
+
+			if ($result)
+				$message = 'OK';
+			else
+				$error = sprintf($lang['dberror'], __FILE__, __LINE__, mysql_error());
+		}
+	}
+}
+
+if ($error)
+{
+?>
+<div id="notification" class="error">
+	<?php echo $error; ?>
+</div>
+<?php
+}
+else
+{
+	if ($message)
+	{
+?>
+<div id="notification" class="success">
+	<?php echo $message; ?>
+</div>
+<?php
+	}
+}
+?>
