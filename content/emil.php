60d9abe786055d3ac9b1101dbd4de69452de438f
flederwiesel <flederwiesel@fra-flugplan.de>
2014-10-15 08:54:02 +0000

    Added support for 'body='.

--- content/emil.php
+++ content/emil.php
@@ -22,6 +22,9 @@ $mail .= ADMIN_EMAIL;
 if (isset($_GET['subject']))
 	$mail .= '&subject='.mb_encode_mimeheader($_GET['subject'], 'ISO-8859-1', 'Q');
 
+if (isset($_GET['body']))
+	$mail .= '&body='.mb_encode_mimeheader($_GET['body'], 'ISO-8859-1', 'Q');
+
 header($mail);
 
 session_start();
