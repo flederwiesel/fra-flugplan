c3bad2daef5788a6fe1dd54f28198b78d72335f0
flederwiesel <flederwiesel@fra-flugplan.de>
2017-01-23 22:07:30 +0000

    Removed sql dead weight.

--- sql/transfer/getflights-2-cargo.sql
+++
@@ -1,6 +0,0 @@
-START TRANSACTION;
-
-ALTER TABLE `flights` CHANGE `type` `type` enum('P', 'F', 'C') NOT NULL DEFAULT 'P';
-ALTER TABLE `history` CHANGE `type` `type` enum('P', 'F', 'C') NOT NULL DEFAULT 'P';
-
-COMMIT;
--- sql/transfer/history/edit_3.sql
+++
@@ -1,1 +0,0 @@
-UPDATE aircrafts SET type=(SELECT id FROM `aircraft-types` WHERE icao='﻿B737') WHERE reg='UR-GBD';
