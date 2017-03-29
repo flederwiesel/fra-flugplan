a644f706beb552827cf74dd66fcd3217664a5df0
flederwiesel <flederwiesel@fra-flugplan.de>
2017-03-29 23:08:49 +0000

    Timezone of ISO 8601 date is now being masked out, as it changes with DST.

--- check/check.sh
+++ check/check.sh
@@ -277,8 +277,8 @@ do
 						s#((Mon|Tues|Wednes|Thurs|Fri|Satur|Sun)day), [0-9]+/[0-9]+/[0-9]+#Day, 00/00/00#g
 						s/((Mon|Diens|Donners|Frei|Sams|Sonn)tag|Mittwoch), [0-9]+\. (Januar|Februar|März|April|Mai|Ju[nl]i|August|(Sept|Nov|Dez)ember|Oktober) [0-9]+/Tag, 00. Monat 0000/g
 						s/^(Date:[ \t]+).+\$/\1Day, 0 Month 0000 00:00:00 +0000/g
-						s/[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}/0000-00-00 00:00:00/g
-						s/[0-9]{4}-[0-9]{2}-[0-9]{2}T/0000-00-00T/g
+						s/[0-9]{4}-[0-9]{2}-[0-9]{2}([ T])[0-9]{2}:[0-9]{2}:[0-9]{2}/0000-00-00\100:00:00/g
+						s/00\+[0-9]{4}/00+0000/g
 						s/token=[0-9a-f]+/token=***/g
 						s/token='[0-9a-f.]+'/token='***'/g
 						s/\(code [0-9]+\)/(code ***)/g
