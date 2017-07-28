4623c1625bd33669e32dd7401d8526d609ffed49
flederwiesel <flederwiesel@fra-flugplan.de>
2017-07-28 11:02:07 +0000

    Added JSON files for cargo. Renamed JSON files. Added .htaccess rule to request downloaded files.

--- www.frankfurt-airport.com/json/curl.sh
+++ www.frankfurt-airport.com/json/curl.sh
@@ -8,23 +8,28 @@ scriptdir=$(dirname "$this")
 mkdir -p "$scriptdir/$version"
 cd "$scriptdir/$version"
 
-# | python -mjson.tool
-curl --silent --noproxy localhost 'http://www.frankfurt-airport.com/de/_jcr_content.aircrafts.json' > flights_copy.aircrafts.json
-curl --silent --noproxy localhost 'http://www.frankfurt-airport.com/de/_jcr_content.airlines.json' > flights_copy.airlines.json
-curl --silent --noproxy localhost 'http://www.frankfurt-airport.com/de/_jcr_content.airports.json' > flights_copy.airports.json
-curl --silent --noproxy localhost 'http://www.frankfurt-airport.com/de/_jcr_content.arrivals.json/filter?time=2015-12-08T10%3A00%3A00%2B01%3A00&perpage=10&lang=de' > flights_copy.arrivals.json
-curl --silent --noproxy localhost 'http://www.frankfurt-airport.com/de/_jcr_content.departures.json/filter?time=2015-12-08T10%3A00%3A00%2B01%3A00&perpage=10&lang=de' > flights_copy.departures.json
+time=$(date +'%Y-%m-%dT%H:%M:%S%z' | sed 's/:/%3A/g;s/\+/%2B/g')
+
+curl --silent --noproxy localhost "https://www.frankfurt-airport.com/de/_jcr_content.aircrafts.json" > aircrafts.json
+curl --silent --noproxy localhost "https://www.frankfurt-airport.com/de/_jcr_content.airlines.json" > airlines.json
+curl --silent --noproxy localhost "https://www.frankfurt-airport.com/de/_jcr_content.airports.json" > airports.json
+curl --silent --noproxy localhost "https://www.frankfurt-airport.com/de/_jcr_content.arrivals.json/filter?perpage=10&lang=de&time=$time" > arrivals.json
+curl --silent --noproxy localhost "https://www.frankfurt-airport.com/de/_jcr_content.departures.json/filter?perpage=10&lang=de&time=$time" > departures.json
+curl --silent --noproxy localhost "https://www.frankfurt-airport.com/de/_jcr_content.arrivals.cargo.json/filter?type=departure&lang=de&time=$time" > arrivals.cargo.json
+curl --silent --noproxy localhost "https://www.frankfurt-airport.com/de/_jcr_content.departures.cargo.json/filter?type=departure&lang=de&time=$time" > departures.cargo.json
 
 mkdir -p beautified
 
 for file in \
-	flights_copy.aircrafts \
-	flights_copy.airlines \
-	flights_copy.airports \
-	flights_copy.arrivals \
-	flights_copy.departures
+	aircrafts \
+	airlines \
+	airports \
+	arrivals \
+	departures \
+	arrivals.cargo \
+	departures.cargo
 do
 	jq . "$file.json" > "beautified/$file.json"
 done
 
-sed -r -i "s:json/[0-9.]+/flights_copy:json/$version/flights_copy:g" "$scriptdir/../.htaccess"
+sed -r -i "s:json/[0-9.]+/:json/$version/:g" "$scriptdir/../.htaccess"
