1d5b7a5ef2d1b01dc6fa6b397c31e9e003572e8f
flederwiesel <flederwiesel@fra-flugplan.de>
2019-06-28 07:23:29 +0000

    Fixed getflights URL and added -L for curl.

--- getflights.sh
+++ getflights.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 date=$(date --rfc-3339=seconds)
-result=$(curl -s https://www.fra-flugplan.de/fra-schedule/getflights.php 2>&1)
+result=$(curl -Ls https://www.fra-flugplan.de/fra-flugplan/getflights.php 2>&1)
 status=$?
 
 if [ $status -eq 0 ]; then
