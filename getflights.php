af3008ce2da378aa859a8e414df7266c440380d7
flederwiesel <flederwiesel@fra-flugplan.de>
2021-07-13 18:16:48 +0000

    Fraport version 1.7.1

--- getflights.php
+++ getflights.php
@@ -1009,7 +1009,7 @@ function JSON_InterpretFlights(/*in*/ $dir, /*in*/ $json, /*in*/ $defer,
 		$result = 0;
 
 		if (isset($obj->version))
-			if (!('1.7' == $obj->version))
+			if (!('1.7.1' == $obj->version))
 				warn_once(__LINE__, "version = $obj->version");
 
 		if ($obj->results > 0)
