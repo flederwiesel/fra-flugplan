f9fb7588f932a36fcb9e85735c35c43b2af51642
flederwiesel <flederwiesel@fra-flugplan.de>
2022-10-22 23:19:51 +0000

    Use HTTPS for link to "airliners.net".

--- photodb.php
+++ photodb.php
@@ -1,7 +1,7 @@
 <?php
 
 $URL = [
-	'airliners.net' => 'http://www.airliners.net/search?sortBy=datePhotographedYear&sortOrder=desc&keywords={reg}',
+	'airliners.net' => 'https://www.airliners.net/search?sortBy=datePhotographedYear&sortOrder=desc&keywords={reg}',
 	'jetphotos.com' => 'https://www.jetphotos.com/showphotos.php?search-type=Advanced&keywords-type=reg&keywords-contain=0&sort-order=2&keywords={reg}',
 	'flugzeugbilder.de' => 'https://www.flugzeugbilder.de/v3/xresult.php?srt=d&ord=descending&rg-srch={reg}',
 	'airfleets.net' => 'https://www.airfleets.net/recherche/?key={reg}'
