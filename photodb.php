14b0fc4c44992fde84fe4c4e978c95aa9f678a95
flederwiesel <flederwiesel@fra-flugplan.de>
2022-12-08 22:25:43 +0000

    Add photodbs netairspace.cc and planespotters.net

--- photodb.php
+++ photodb.php
@@ -5,6 +5,8 @@ $URL = [
 	'airliners.net' => 'https://www.airliners.net/search?sortBy=datePhotographedYear&sortOrder=desc&keywords={reg}',
 	'flugzeugbilder.de' => 'https://www.flugzeugbilder.de/v3/xresult.php?srt=d&ord=descending&rg-srch={reg}',
 	'jetphotos.com' => 'https://www.jetphotos.com/showphotos.php?search-type=Advanced&keywords-type=reg&keywords-contain=0&sort-order=2&keywords={reg}',
+	'netairspace.cc' => 'https://www.netairspace.cc/photos/search.php?search=Search&presentation=info&sortorder=latestfirst&registration={reg}',
+	'planespotters.net' => 'https://www.planespotters.net/search?q={reg}',
 ];
 
 ?>
