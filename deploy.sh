d0b392833124e39916c284e938d86e848952d31a
flederwiesel <flederwiesel@fra-flugplan.de>
2023-10-21 01:21:53 +0200

    Replace svn references in `deploy.sh` with git.

--- deploy.sh
+++ deploy.sh
@@ -17,50 +17,28 @@
 root=httpdocs/vault/fra-flugplan
 
 cd $(dirname "$0")
-sed -i "s#^[^/]*define[ \t]*([ \t]*'DEBUG'#//&#g" .config
 
-svn upgrade
-svn update
+git diff-index --quiet HEAD -- ||
+{
+	echo "Working copy is dirty. Aborting." >&2
+	exit 1
+}
 
-info=$(LC_MESSAGES=en_US svn info . 2>&1)
-
-if [ $? -ne 0 ]; then
-	echo -e "\033[1;31m$info\033[m"
+if ! tag=$(git describe --tags --exact-match); then
+	echo "Cannot determine tag." >&2
 	exit 1
 fi
-
-rev=$(awk '/^Revision:/ { print $2; }' <<<"$info")
-
-if [ -z "$rev" ]; then
-	echo -e "\033[1;31mNo revision found:\033[m"
-	echo -e "\033[1;31m$info\033[m"
-	exit 1
-else
-	echo -e "\033[36mRevision: $rev\033[m"
-fi
-
-LC_MESSAGES=en_US svn info . | awk '/^Last Changed (Rev|Date):/ { print $0; }' > revision
-(
-LC_MESSAGES=en_US svn info . | awk '/^URL:/ { print $2; }'
-echo '------------------------------------------------------------------------'
-LC_MESSAGES=en_US svn log | sed -nr 'H; :a /---/ { x; s/\([a-z]+,[^)]+\) //g; s/[0-9]+ lines?//g; s/-*-$//g; s/\n//g; p; n; h; ba }'
-) > history
 
 # If checked out under Windows, cygwin permissions are wrong, which
 # may lead to permissions being wrong on the server.
 
-find -type d -print0 | xargs -0 chmod 755
-find -type f -print0 | xargs -0 chmod 644
-IFS=$'\n' executable=($(svn propget -R svn:executable | sed 's/ - \*//g'))
-chmod 755 "${executable[@]}"
-
 ssh fra-flugplan.de "mkdir-p $HOME/httpdocs/vault/fra-flugplan $HOME/httpdocs/var/log"
-ssh fra-flugplan.de "rm -rf $root/$rev"
+ssh fra-flugplan.de "rm -rf $root/$tag"
 rsync -av  \
 --exclude=*.dsk \
 --exclude=*.ppw \
 --exclude=.phped \
---exclude=.svn \
+--exclude=.git \
 --exclude=api.stopforumspam.org \
 --exclude=check \
 --exclude=etc \
@@ -69,11 +47,6 @@ rsync -av  \
 --exclude=METAR \
 --exclude=sql \
 --exclude=www.frankfurt-airport.com \
-. fra-flugplan.de:"$root/$rev"
+. fra-flugplan.de:"$root/$tag"
 
-ssh fra-flugplan.de "echo $rev > $root/target"
-
-svn revert revision
-rm history
-
-echo -e "\033[36mRevision: $rev\033[m"
+ssh fra-flugplan.de "echo $tag > $root/target"
