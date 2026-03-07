082de719ea0597b196c46666065df71c08ea7e08
flederwiesel <flederwiesel@fra-flugplan.de>
2026-03-07 16:45:26 +0100

    Fix import script, add xz.

--- sql/import.sh
+++ sql/import.sh
@@ -2,34 +2,21 @@
 
 readonly SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
 
-export PATH="${SCRIPTDIR}/../bin:$PATH"
-
-sqlexec() {
-	[[ "$(uname -o)" =~ "Cygwin" ]] && proto="--protocol=TCP"
-	mysql --host=localhost $proto --user=flugplan --password="$(getpass machine=mysql://localhost login=flugplan)" --default-character-set=utf8 "$@"
-}
-
 if [ $# -lt 1 ]; then
 	echo "Usage: $(basename $0) <file>"
 else
 	case "${1##*.}" in
-	'bz2')
-		cmd="bunzip2 -c '$1'"
-		;;
-	'gz')
-		cmd="gunzip -c '$1'"
-		;;
 	'sql')
-		cmd="cat '$1'"
+		cat "$1"
 		;;
-	'zip')
-		cmd="unzip -cp '$1'"
+	'xz')
+		xzcat "$1"
 		;;
 	*)
 		echo "Don't know what to do for ${1##*.}." >&2
 		exit 1
 		;;
-	esac
-
-	sqlexec <<< "$(eval $cmd | sed 's/flederwi_/flederwiesel_/g')"
+	esac |
+	sed '1 s/!999999\\-//g' |
+	mysql --default-character-set=utf8
 fi
