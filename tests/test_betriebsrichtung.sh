dbb059a3408d26e606a20e2e83ed38366fbd245e
flederwiesel <flederwiesel@fra-flugplan.de>
2026-03-08 17:05:20 +0100

    Remove superfluous code.

--- tests/test_betriebsrichtung.sh
+++ tests/test_betriebsrichtung.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
 
-findroot() {
-	name=$(dirname "$1")
-
-	while [ -n "$name" ]
-	do
-		name=$(readlink -f "$name")
-
-		if [ "/" == "$name"  ]; then
-			break
-		else
-			if [ -f "$name/.htaccess" ]; then
-					root="$name"
-			fi
-		fi
-
-		name="$name/.."
-	done
-
-	echo "$root"
-}
-
-datadir=$(findroot "${BASH_SOURCE[0]}")
-
 check "1" browse "$url/sslapps.fraport.de/betriebsrichtung/betriebsrichtungsvg.js"
